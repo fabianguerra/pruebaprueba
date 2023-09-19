ecb135c00057d093210ad7e04ab8f2570dba8864
Fabian <fabian@live.com>
2023-09-19 16:55:53 -0500

    imagen 2

--- src/componente/PokemonId/hooks/usePokemonId.tsx
+++ src/componente/PokemonId/hooks/usePokemonId.tsx
@@ -10,7 +10,7 @@ const useGetPokemonId = () => {
   }, []);
 
   const getCharizard = async () => {
-    const respuesta = await axios.get("https://pokeapi.co/api/v2/pokemon/11");
+    const respuesta = await axios.get("https://pokeapi.co/api/v2/pokemon/6");
     const datainfo = respuesta.data;
     const {id, name,sprites, weight} = datainfo;
     const imagen = sprites.front_shiny;
